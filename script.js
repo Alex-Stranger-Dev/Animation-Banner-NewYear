ced2f5aff60462fff88a992107561cf50c2b51ed
Alex <118556086+Alex-Stranger-Dev@users.noreply.github.com>
2024-03-11 17:30:01 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,32 @@
+let canvas = document.querySelector("#canvas");
+
+let ctx = canvas.getContext("2d");
+let winWidth = window.innerWidth;
+let winHeight = window.innerHeight;
+
+//Set Canvas Background Color;
+canvas.width = winWidth;
+canvas.height = winHeight;
+ctx.fillStyle = "#000";
+ctx.fillRect(0, 0, winWidth, winHeight);
+
+//Glow effect;
+ctx.shadowBlur = 10;
+ctx.shadowColor = "#fff";
+
+function animate() {
+  //Random position and size of stars;
+  let x = winWidth * Math.random();
+  let y = winHeight * Math.random();
+  let r = 2.5 * Math.random();
+
+  //Draw the stars;
+  ctx.beginPath();
+  ctx.fillStyle = "#fff";
+  ctx.arc(x, y, r, 0, Math.PI * 2);
+  ctx.fill();
+
+  setTimeout(animate, 100);
+}
+
+animate();
